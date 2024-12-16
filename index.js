23099b380380d28dbfb20a273daa8bc09304f60e
rllynotfox <rllynotfox@gmail.com>
2024-12-16 04:45:14 +0300

    господи насколько же я хочу спать я надеюсь это примут на тройку хотя бы

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-password = "1234"
+let password
+
+if (localStorage.getItem("password")) {
+    password = localStorage.getItem("password")
+} else {
+    password = "admin"
+    localStorage.setItem("password", "admin")
+}
+
+document.getElementById("select1").addEventListener("click", (ev) => {
+    if (document.getElementById("select1").classList.contains("cdelete")) {
+        document.getElementById("teacher1").classList.add("cfree")
+        document.getElementById("teacher1").innerText = "Кабинет свободен"
+        document.getElementById("select1").classList.remove("cdelete")
+        document.getElementById("select1").innerText = "Занять"
+    }  else {
+        document.getElementById("teachers-wrapper").style.display = "flex"
+    }
+})
+
+document.getElementById('select-teach1').addEventListener("click", (ev) => {
+    document.getElementById("select1").classList.add("cdelete")
+    document.getElementById("teacher1").innerText = "Платонов Вячеслав Дмитреивич"
+    document.getElementById("teachers-wrapper").style.display = "none"
+    document.getElementById("teacher1").classList.remove("cfree")
+
+
+})
+
+document.getElementById("teachers-close").addEventListener("click",(ev) => {
+    document.getElementById("teachers-wrapper").style.display = "none"
+
+})
 
 document.getElementById("settings-o").addEventListener("click", (ev) => {
     document.getElementById("pass-wrapper").style.display = "flex"
@@ -11,9 +43,36 @@ document.getElementById("pass-close").addEventListener("click", (ev) => {
 
 })
 
+document.getElementById("save-password").addEventListener("click", (ev) => {
+    password = document.getElementById("newpass-field").value
+    localStorage.setItem("password", password)
+    alert("Пароль обновлен")
+    document.getElementById("newpass-field").value = ""
+})
+
+document.getElementById("cab-list").addEventListener("click", (ev) => {
+    document.getElementById("settings-wrapper").style.display = "none"
+    document.getElementById("cab-wrapper").style.display = "flex"
+
+}) 
+
+document.getElementById("teacher-list").addEventListener("click", (ev) => {
+    document.getElementById("settings-wrapper").style.display = "none"
+    document.getElementById("teacher-wrapper").style.display = "flex"
+
+}) 
+
+
+
+document.getElementById("settings-close").addEventListener("click", (ev) => {
+    document.getElementById("settings-wrapper").style.display = "none"
+
+})
+
 document.getElementById("pass-go").addEventListener("click", (ev) => {
     if (document.getElementById("pass-input").value == password) {
-        alert("ok")
+        // alert("ok")
+        document.getElementById("settings-wrapper").style.display = "flex"
         document.getElementById("pass-wrapper").style.display = "none"
         document.getElementById("pass-alert").style.display = "none"
         document.getElementById("pass-input").value = ""
